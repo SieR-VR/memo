0c17fb1b8d369046fc08c71a7d1fd6222156824a
Bae Junhyeong <39817813+Lambda1357@users.noreply.github.com>
2022-03-20 23:12:57 +0900

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,15 @@
+import adapter from '@sveltejs/adapter-node';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
-
-import node from '@sveltejs/adapter-node';
-
 const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
 	kit: {
-		adapter: node(),
-		
+		adapter: adapter(),
+
 		// Override http methods in the Todo forms
 		methodOverride: {
 			allowed: ['PATCH', 'DELETE']
